378d17cf9cd36c7f5b1cda161c070ece44fc016a
Philippa Davies <philippa.davies@bristol.ac.uk>
2026-02-02 16:48:34 +0000

    updated to use rpackage

--- scripts/predict-proteins.r
+++ scripts/predict-proteins.r
@@ -2,11 +2,10 @@
 ## predict-proteins.r
 ## -------------------------------------------- ##
 library(meffonym)
+library(here)
+library(rhds.rpackage)
 
-my.write.table <- function(x, filename) {
-  cat("saving", basename(filename), "...\n")
-  write.table(x, file = filename, row.names = T, col.names = T, sep = "\t")
-}
+
 
 args <- commandArgs(trailingOnly = T)
 datadir <- args[1]
